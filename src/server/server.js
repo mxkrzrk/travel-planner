5aab8b2203de50a239484bf78fb5ee3bb57b231b
Massimilianok <massimiliano.rizzuto87@gmail.com>
2020-01-16 00:03:50 +0100

    Add express server

---
+++ src/server/server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+// Configurations server
+const app = express();
+const port = 8000;
+app.listen(port, () => console.log(`Server running on port: ${port}`));
+
+// Middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cors());
+
+// Serves static folder
+app.use(express.static("./src/client/views/"));
